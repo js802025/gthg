88b9ab831630d98c729eeec425c9538ec84fc6ac
js802025 <30957566+js802025@users.noreply.github.com>
2022-08-25 22:41:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var firebase = require("firebase-admin");
 var firebase_details = require("./gthg-8b42f-firebase-adminsdk-mdi6c-19234d5833.json")
 console.log(process.env.PRIVATE_KEY)
 firebase_details.private_key_id = process.env.PRIVATE_KEY_ID
-firebase_details.private_key = process.env.PRIVATE_KEY
+firebase_details.private_key = JSON.parse(process.env.PRIVATE_KEY)
 
 
 firebase.initializeApp({
@@ -211,7 +211,7 @@ async function submittedUids(callback) {
   const {GoogleSpreadsheet} = require('google-spreadsheet');
   var gc_details = require('./client_secret.json')
   gc_details.private_key_id = process.env.PRIVATE_KEY_ID
-  gc_details.private_key = process.env.GC_PRIVATE_KEY
+  gc_details.private_key = JSON.parse(process.env.GC_PRIVATE_KEY)
 
   
 
@@ -236,7 +236,7 @@ async function addTurtleLocation(user, location, callback) {
   const {GoogleSpreadsheet} = require('google-spreadsheet');
   var gc_details = require('./client_secret.json')
   gc_details.private_key_id = process.env.PRIVATE_KEY_ID
-  gc_details.private_key = process.env.GC_PRIVATE_KEY
+  gc_details.private_key = JSON.parse(process.env.GC_PRIVATE_KEY)
 
   
 
